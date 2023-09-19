9911d9014403c9c6ab8b4aab459f7a0457aee8fd
hbc03 <hboracelebi2003@gmail.com>
2023-09-18 22:28:48 -0400

    multiset imp

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -1,2 +1,36 @@
-public class MultiSet {//aba
+public class MultiSet {
+    public boolean add(Object item) {
+        return false; // Placeholder return value
+    }
+
+    // Remove the specified item from the collection.
+    public void remove(Object item) {
+        // Your implementation here
+    }
+
+    // Check if the collection contains the specified item and return true if found, false otherwise.
+    public boolean contains(Object item) {
+        // Your implementation here
+        return false; // Placeholder return value
+    }
+
+    // Check if the collection is empty and return true if it is, false otherwise.
+    public boolean isEmpty() {
+        // Your implementation here
+        return false; // Placeholder return value
+    }
+
+    // Count the number of occurrences of the specified item in the collection.
+    public int count(Object item) {
+        // Your implementation here
+        return 0; // Placeholder return value
+    }
+
+    // Get the size (number of elements) of the collection.
+    public int size() {
+        // Your implementation here
+        return 0; // Placeholder return value
+    }
+
+
 }
